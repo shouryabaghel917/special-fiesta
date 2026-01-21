8a73dee9ca4dc5197d4a314e70d557a0d136a40f
Shourya Baghel <shouryabaghel917@proton.me>
2026-01-21 15:06:19 +0530

    Create container.tsx

---
+++ src/components/ui/container.tsx
@@ -0,0 +1,13 @@
+import { cn } from "@/lib/cn";
+
+export function Container({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn("mx-auto w-full max-w-6xl px-4 md:px-6", className)}
+      {...props}
+    />
+  );
+}
